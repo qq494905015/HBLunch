09b0a52f85331e472169d93ddb03a818c41d9236
heguokang <heguokang@foresee.com.cn>
2017-09-12 23:01:25 +0800

    建立order模块

---
+++ src/components/project/order/Order.vue
@@ -0,0 +1,20 @@
+<template>
+  <div >
+    订单管理
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Order',
+  data () {
+    return {
+      msg: '订单管理'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Order from '@/components/project/order/Order'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/Order',
+      name: 'Order',
+      component: Order
     }
   ]
 })
