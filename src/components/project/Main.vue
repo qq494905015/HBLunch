92910488da067fa3bdf309b7d53dc6c5de34986e
heguokang <xhqb@520>
2017-10-24 19:50:04 +0800

    dsdfdf

--- src/components/project/Main.vue
+++ src/components/project/Main.vue
@@ -1,14 +1,22 @@
 <template>
   <div id="main">
     <el-menu theme="dark" :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-      <router-link :to="{path:'Analysis'}"><el-menu-item index="analysis">分析统计 </el-menu-item></router-link>
-      <el-submenu index="menu">
-        <template slot="title">菜单管理</template>
-        <router-link :to="{path:'Menu_class'}"><el-menu-item index="menu_class">菜品分类管理</el-menu-item></router-link>
-        <router-link :to="{path:'Menu_dishes'}"><el-menu-item index="menu_dishes">菜品管理</el-menu-item></router-link>
-      </el-submenu>
-      <router-link :to="{path:'OrderManager'}"><el-menu-item index="order">订单管理</el-menu-item></router-link>
-    </el-menu>
+    <router-link :to="{path:'Analysis'}">
+      <el-menu-item index="analysis">分析统计 </el-menu-item>
+    </router-link>
+    <el-submenu index="menu">
+      <template slot="title">菜单管理</template>
+      <router-link :to="{path:'Menu_class'}">
+        <el-menu-item index="menu_class">菜品分类管理</el-menu-item>
+      </router-link>
+      <router-link :to="{path:'Menu_dishes'}">
+        <el-menu-item index="menu_dishes">菜品管理</el-menu-item>
+      </router-link>
+    </el-submenu>
+    <router-link :to="{path:'OrderManager'}">
+      <el-menu-item index="order">订单管理</el-menu-item>
+    </router-link>
+  </el-menu>
   </div>
 </template>
 
